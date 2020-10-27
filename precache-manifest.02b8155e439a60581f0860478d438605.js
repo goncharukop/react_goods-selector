5a0145a146a1df1f6aed62c6fd6a48fed31a0fa0
goncharukop <goncharukop@gmail.com>
2020-10-27 23:15:46 +0200

    Updates

---
+++ precache-manifest.02b8155e439a60581f0860478d438605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57f00fb9a833f9f885c1bb65bb82c37",
+    "url": "./index.html"
+  },
+  {
+    "revision": "555aef754b9e8b55b1ee",
+    "url": "./static/css/main.f9b649ab.chunk.css"
+  },
+  {
+    "revision": "14695e46ea38ee5074a9",
+    "url": "./static/js/2.7f413de1.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.7f413de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555aef754b9e8b55b1ee",
+    "url": "./static/js/main.425fd139.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
